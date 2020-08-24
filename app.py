2ef7827cc06069672b2fccb97e18f4fbfe008ba5
cedric <conolcedric@gmail.com>
2020-08-24 21:37:10 +0800

    streamlit app.py

--- app.py
+++ app.py
@@ -1,25 +1,29 @@
 import streamlit as st
-
-from bertosaurus import functions
+from bertosaurus import Bertosaurus
 
 st.title("Bertosaurus")
 st.header("The better thesaurus.")
 
-
-# check if bert is present
-@st.cache
-def check():
-    functions.check_model_status()
-
-
-# load models
 @st.cache(allow_output_mutation=True)
-def load_models():
-    bert = functions.load_model("en_trf_bertbaseuncased_lg")
-    nlp = functions.load_model("en_core_web_sm")
+def create_obj():
+    thesaurus = Bertosaurus()
+    return thesaurus
 
-    return bert, nlp
+# # check if bert is present
+# @st.cache
+# def check():
+#     functions.check_model_status()
 
+
+# # load models
+# @st.cache(allow_output_mutation=True)
+# def load_models():
+#     bert = functions.load_model("en_trf_bertbaseuncased_lg")
+#     nlp = functions.load_model("en_core_web_sm")
+
+#     return bert, nlp
+
+thesaurus = create_obj()
 
 sentence = st.text_input("Input sentence", "I want to change the world.")
 word = st.text_input("Word in sentence", "change")
@@ -36,11 +40,11 @@ elif not word:
     st.warning("Please write the word to start searching for synonyms.")
 else:
 
-    bert, nlp = load_models()
+    # bert, nlp = load_models()
 
     st.subheader("Suggested synonyms:")
-    syns = functions.ranked_synonyms(sentence, word, nlp, bert)
+    syns = thesaurus.ranked_synonyms(sentence, word)
 
     for syn, score in syns:
-        text = syn + " ({}% match)".format(score)
+        text = syn + " ({}% match)".format(score*100)
         st.write(text)
